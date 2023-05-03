d4f83afef5918645dd410bb774231c64d1b499f8
Noah Kim <dongwoo_333@hanmail.net>
2023-05-04 08:55:28 +1000

    32. Writing the Palette Endpoint

--- 04-prj-color-palette-generator/ColorPaletteFlaskApp/app.py
+++ 04-prj-color-palette-generator/ColorPaletteFlaskApp/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
 import openai
 from dotenv import dotenv_values
+import json
 
-config = dotenv_values("../.env")
+config = dotenv_values("./.env")
 openai.api_key = config["OPENAI_API_KEY"]
 
 
@@ -12,12 +13,55 @@ app = Flask(
 )
 
 
+def print_completion_token_usage(response):
+    """
+    This function prints the token usage of the Complete response.
+    It works for Completion and ChatCompletion
+    """
+    # "·Token usage: 36 = 33 + 3 (prompt + completion)"
+    print(
+        f"·Token usage: {response['usage']['total_tokens']} = {response['usage']['prompt_tokens']} + {response['usage']['completion_tokens']} (prompt + completion)")
+
+
+def get_colors(msg):
+    prompt = f"""
+    You are a color palette generating assistant that responds to text prompts for color palettes
+    Your should generate color palettes that fit the theme, mood, or instructions in the prompt.
+    The palettes should be between 2 and 8 colors.
+
+    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea
+    A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
+
+    Q: Convert the following verbal description of a color palette into a list of colors: sage, nature, earth
+    A: ["#EDF1D6", "#9DC08B", "#609966", "#40513B"]
+
+
+    Desired Format: a JSON array of hexadecimal color codes
+
+    Q: Convert the following verbal description of a color palette into a list of colors: {msg} 
+    A:
+    """
+
+    response = openai.Completion.create(
+        prompt=prompt,
+        model="text-davinci-003",
+        max_tokens=200,
+    )
+
+    print_completion_token_usage(response)
+    colors = json.loads(response["choices"][0]["text"])
+    return colors
+
+
 @app.route('/palette', methods=['POST'])
 def prompt_to_palette():
-    # OPEN AI COMPLETION CALL
+    query = request.form.get("query")
+    app.logger.info(query)
 
-    # RETURN LIST OF COLORS
-    print("test")
+    # OPEN AI COMPLETION CALL
+    colors = get_colors(query)
+
+    return {"colors": colors}
 
 
 @app.route('/')
