99c2b0dc6e11f2f0b5b23cc51883e8c42e5dfc68
Noah Kim <dongwoo_333@hanmail.net>
2023-05-07 08:34:51 +1000

    53. Persisting Messages Across Requests

--- 07-chatbot/chatbot.py
+++ 07-chatbot/chatbot.py
@@ -4,17 +4,21 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
+messages = []
 
 while True:
     try:
         user_input = input("You: ")
+        messages.append({"role": "user", "content": user_input})
+
         res = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": user_input},
-            ]
+            messages=messages
         )
-        print(res["choices"][0]["message"])
+
+        messages.append(res["choices"][0]["message"].to_dict())
+        # print(messages)
+        print("Assistant: ", res["choices"][0]["message"]["content"])
 
     except KeyboardInterrupt:
         print("Exiting...")
