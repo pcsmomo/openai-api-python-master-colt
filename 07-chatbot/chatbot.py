4f0e74ca25f3b854ffb4b8fa2462bc9dbbb19f62
Noah Kim <dongwoo_333@hanmail.net>
2023-05-07 08:53:57 +1000

    54. Adding Optional Personalities

--- 07-chatbot/chatbot.py
+++ 07-chatbot/chatbot.py
@@ -1,29 +1,53 @@
 import openai
 from dotenv import dotenv_values
+import argparse
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
-messages = []
 
-while True:
-    try:
-        user_input = input("You: ")
-        messages.append({"role": "user", "content": user_input})
+def main():
+    parser = argparse.ArgumentParser(
+        description="Simple command line chatbot with GPT")
 
-        res = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages
-        )
+    parser.add_argument("--personality",
+                        type=str,
+                        help="A brief summary of the chatbot's personality",
+                        default="friendly and helpful")
 
-        res_message = res["choices"][0]["message"]
-        messages.append(res_message.to_dict())
-        # print(messages)
-        print("Assistant: ", res_message["content"])
-        print("\n\n")
+    args = parser.parse_args()
+    # python chatbot.py --personality "silly and goofy"
+    # python chatbot.py --personality "rude and sarcastic"
+    # python chatbot.py --personality "chlidish and toddler-like 3 years old"
+    # You: What should I eat for dinner?
+    # print(args)
+    # Namespace(personality='silly and goofy')
 
-    except KeyboardInterrupt:
-        print("Exiting...")
-        break
+    initial_prompt = f"You are a conversational chatbot. Your personality is {args.personality}"
+    messages = [{"role": "system", "content": initial_prompt}]
 
-print(res)
+    while True:
+        try:
+            user_input = input("You: ")
+            messages.append({"role": "user", "content": user_input})
+
+            res = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages
+            )
+
+            res_message = res["choices"][0]["message"]
+            messages.append(res_message.to_dict())
+            # print(messages)
+            print("Assistant: ", res_message["content"])
+            print("\n")
+
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
+
+    # print(res)
+
+
+if __name__ == "__main__":
+    main()
